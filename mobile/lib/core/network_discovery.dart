21ab2caaf9be2e4b75767c110b6c04cf22dd99fa
dblademasteh <dblademasteh@users.noreply.github.com>
2026-07-24 14:21:49 +0800

    feat: Flutter web build with Dockerfile, backend URL set to Pi public IP

--- mobile/lib/core/network_discovery.dart
+++ mobile/lib/core/network_discovery.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 
 class NetworkDiscovery {
   static Future<String> detect() async {
-    if (kIsWeb) return 'http://localhost:4000';
+    if (kIsWeb) return 'http://49.150.192.176:4000';
 
     final backend = await _scanForBackend();
     if (backend != null) return backend;
